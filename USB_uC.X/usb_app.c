3b308b0dbc1b0988b53c24dd5dccee33e39cea7c
johnnydrazzi <johnny.drazzi@gmail.com>
2024-01-26 15:43:29 +1100

    Update to latest API used.

--- USB_uC.X/usb_app.c
+++ USB_uC.X/usb_app.c
@@ -2,10 +2,10 @@
  * @file usb_app.c
  * @brief Contains application specific functions.
  * @author John Izzard
- * @date 12/06/2020
+ * @date 26/01/2024
  * 
  * USB uC - USB Application file (This file is for MSD Bootloader).
- * Copyright (C) 2017-2020  John Izzard
+ * Copyright (C) 2017-2024  John Izzard
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,6 @@
 #include "usb.h"
 #include "usb_app.h"
 #include "usb_msd.h"
-#include "usb_ch9.h"
 
 
 bool usb_service_class_request(void)
@@ -32,15 +31,17 @@ bool usb_service_class_request(void)
     return msd_class_request();
 }
 
-bool usb_get_class_descriptor(void)
+bool usb_get_class_descriptor(const uint8_t** descriptor, uint16_t* size)
 {
     return false; // No class descriptors for MSD.
 }
+
 
 void usb_app_init(void)
 {
     msd_init();
 }
+
 
 void usb_app_tasks(void)
 {
@@ -52,32 +53,34 @@ void usb_app_tasks(void)
     }
 }
 
+
 void usb_app_clear_halt(uint8_t bd_table_index, uint8_t ep, uint8_t dir)
 {
     msd_clear_halt(bd_table_index, ep, dir);
 }
 
+
 bool usb_app_set_interface(uint8_t alternate_setting, uint8_t interface)
 {
 #if NUM_ALT_INTERFACES != 0
-    if(g_usb_set_interface.Interface < NUM_INTERFACES) return false;
+    if(g_usb_set_interface.Interface < NUM_INTERFACES) return true;
 #else
-    if(alternate_setting == 0 && interface == 0)
-    {
-        msd_clear_ep_toggle();
-        return true;
-    }
-    else return false;
+    if(alternate_setting != 0 || interface != 0) return false;
+    
+    msd_clear_ep_toggle();
+    return true;
 #endif
 }
+
 
 bool usb_app_get_interface(uint8_t* alternate_setting_result, uint8_t interface)
 {
 #if NUM_ALT_INTERFACES != 0
-    if(g_get_interface.Interface < NUM_INTERFACES) return false;
+    if(g_get_interface.Interface < NUM_INTERFACES) return true;
 #endif
     return false;
 }
+
 
 bool usb_out_control_finished(void)
 {

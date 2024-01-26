dd146692c016af020548afb4e377b536a46db099
johnnydrazzi <johnny.drazzi@gmail.com>
2024-01-26 15:41:27 +1100

    Remove _XTAL_FREQ define

--- USB_uC.X/config.h
+++ USB_uC.X/config.h
@@ -44,7 +44,6 @@
 #define MHz_48 12
 #endif
 
-#define _XTAL_FREQ          48000000      // Don't modify.
 #define PLL_STARTUP_DELAY() __delay_ms(3) // Don't modify.
 
 // PIC16F145X Settings:
@@ -200,8 +199,8 @@
 //#define LED_ACTIVE_LOW    // Uncomment to make the Bootloader LED active low.
 #endif
 
-// PIC18F14K50 Settings:
-#elif defined(_18F14K50)
+// PIC18F13K50 & PIC18F14K50 Settings:
+#elif defined(_18F13K50) || defined(_18F14K50)
 #define DM164127  0 // Compatible with DM164127-2/DV164139-2 https://www.microchip.com/developmenttools/ProductDetails/PartNO/DM164127-2
 #define GENERAL   1 // Compatible with dev boards that have a reset button.
 #define DEV_BOARD 2 // A custom dev board I use.
